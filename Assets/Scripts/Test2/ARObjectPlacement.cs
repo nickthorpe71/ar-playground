5527de7775b37a9d56c4312c843be46e0010beb5
nickthorpe71 <nickthorpe71@gmail.com>
2020-07-24 07:22:29 -0500

    after rollback packages stopped working need to switch back to 2020 branch

--- Assets/Scripts/Test2/ARObjectPlacement.cs
+++ Assets/Scripts/Test2/ARObjectPlacement.cs
@@ -1,18 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARSubsystems;
 
 public class ARObjectPlacement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject placementIndictor;
+
+    private ARRaycastManager rayManager;
+    private Pose placementPose; //simple data structure that describes the position and rotation of a 3D point
+    private bool placementPoseIsValid = false;
+
     void Start()
     {
-        
+        rayManager = FindObjectOfType<ARRaycastManager>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+    }
+
+    void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        rayManager.Raycast(screenCenter, hits, TrackableType.Planes);
+
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+        }
+
+    }
+
+    void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+            placementIndictor.SetActive(true);
+            placementIndictor.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndictor.SetActive(false);
+        }
     }
 }
